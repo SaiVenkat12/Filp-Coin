1806533433d7452de6b72347e6d2d7e353cb6547
SaiVenkat12 <gsaiv1001@gmail.com>
2022-11-21 00:37:52 +0530

    [SaiVenkat]

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,19 @@
-#!/bin/bash -x
+#!/bin/bash
+
+h=0;
+t=0;
+for (( n=0; n<21; n++))
+do
 
 a=$(($RANDOM%2));
-echo "$a"
 
 if [ $a -eq 1 ]
 then
-        echo "heads";
+	h=$(( h + 1))
 else
-        echo "tails;"
+	t=$(( t + 1))
 fi
+done
+
+echo "heads $h times";
+ echo "tails $t times"
